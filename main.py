7de3b2e8fe6bae444b27eb710372560010aa6dbc
GabrielaPalmeiraSilva <gabrielapalmeirasilva2510@gmail.com>
2026-09-14 15:53:06 +0000

    Fase 2

--- main.py
+++ main.py
@@ -4,18 +4,23 @@ startup = {
     "ano_adesao": "2026"
 }
 solucoes_ativas = ["Firewall IA", "Scan de Vulnerabilidades"]
-print(f"Startup: {startup['nome']} - Segmento: {startup['segmento']}")
-print(f"Primeiro produto: {solucoes_ativadas[0]}")
+print("CADASTRO DE STARTUP")
+print(f"Startup: {startup['nome']}")
+print(f"Segmento: {startup['segmento']}")
+print(f"Primeiro produto: {solucoes_ativas[0]}")
+print("-" * 30 + "\n")
 
 bancadas = [
     [1,0],
-    [0,1],
+    [0,1]
 ]
-print("\nMapeamento das Bancadas - Legenda: 1 = Ocupado e 0 = Livre")
+print("Mapeamento das Bancadas")
+print("Legenda: 1 = Ocupado e 0 = Livre\n")
 print(f"Bancada N1 [0][0]: {bancadas[0][0]}")
 print(f"Bancada N2 [0][1]: {bancadas[0][1]}")
 print(f"Bancada S1 [1][0]: {bancadas[1][0]}")
 print(f"Bancada S2 [1][1]: {bancadas[1][1]}")
+print("-" * 30)
 
 with open("custos_cloud.csv", "r", encoding="utf-8") as arquivo:
     cabecalho = arquivo.readline()
@@ -29,3 +34,16 @@ print(linha_1, end="")
 print(linha_2, end="")
 print(linha_3, end="")
 print(linha_4, end="")
+
+custos = [
+    float(linha_1.split(",")[1].strip()),
+    float(linha_2.split(",")[1].strip()),
+    float(linha_3.split(",")[1].strip()),
+    float(linha_4.split(",")[1].strip())
+]
+total_custos = sum(custos)
+
+print("\nPAINEL FINAL")
+print(f"Startup: {startup['nome']}")
+print("Bancada alocada: Bancada N1")
+print(f"Total da infraestrutura em nuvem: R$ {total_custos:.2f}")
