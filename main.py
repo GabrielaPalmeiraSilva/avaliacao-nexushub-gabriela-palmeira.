20dc325fde078955c3c8d5c0ebd63b9a6adf9ef0
GabrielaPalmeiraSilva <gabrielapalmeirasilva2510@gmail.com>
2026-09-14 14:28:01 +0000

    etapa 1

--- main.py
+++ main.py
@@ -16,3 +16,16 @@ print(f"Bancada N1 [0][0]: {bancadas[0][0]}")
 print(f"Bancada N2 [0][1]: {bancadas[0][1]}")
 print(f"Bancada S1 [1][0]: {bancadas[1][0]}")
 print(f"Bancada S2 [1][1]: {bancadas[1][1]}")
+
+with open("custos_cloud.csv", "r", encoding="utf-8") as arquivo:
+    cabecalho = arquivo.readline()
+    linha_1 = arquivo.readline()
+    linha_2 = arquivo.readline()
+    linha_3 = arquivo.readline()
+    linha_4 = arquivo.readline()
+
+print(cabecalho, end="")
+print(linha_1, end="")
+print(linha_2, end="")
+print(linha_3, end="")
+print(linha_4, end="")
