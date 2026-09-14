bee52d2ba05ada4c7b0a34f5186c8f791e0a0f9d
GabrielaPalmeiraSilva <gabrielapalmeirasilva2510@gmail.com>
2026-09-14 13:06:32 +0000

    etapa 1

---
+++ main.py
@@ -0,0 +1,18 @@
+startup = {
+    "nome": "CyberPulse Tech",
+    "segmento": "Segurança da Informação",
+    "ano_adesao": "2026"
+}
+solucoes_ativas = ["Firewall IA", "Scan de Vulnerabilidades"]
+print(f"Startup: {startup['nome']} - Segmento: {startup['segmento']}")
+print(f"Primeiro produto: {solucoes_ativadas[0]}")
+
+bancadas = [
+    [1,0],
+    [0,1],
+]
+print("\nMapeamento das Bancadas - Legenda: 1 = Ocupado e 0 = Livre")
+print(f"Bancada N1 [0][0]: {bancadas[0][0]}")
+print(f"Bancada N2 [0][1]: {bancadas[0][1]}")
+print(f"Bancada S1 [1][0]: {bancadas[1][0]}")
+print(f"Bancada S2 [1][1]: {bancadas[1][1]}")
